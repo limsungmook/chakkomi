19e179db9e77476c896633d5a696b4e91dfc2d1e
LimSungMook <ipes4579@gmail.com>
2011-09-01 20:12:06 +0000

    Chakkomi init commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Chakkomi::Application.config.secret_token = 'afd352c72b05a59ae05fd9e1e13628149b8e9923351e4b656c241e36bcb1e618cbcae68167915bdf246cb178a15e83210de4e0bb9cf71560f638f385aded2fdb'
