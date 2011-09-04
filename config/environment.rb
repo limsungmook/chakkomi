e062ae8d85828598655b7b9616df1169cb18e41a
SungMook Lim <ipes4579@gmail.com>
2011-09-05 04:40:29 +0900

    트위터 키 추가

--- config/environment.rb
+++ config/environment.rb
@@ -1,23 +1,23 @@
 # Load the rails application
 DEFAULT_PAPERCLIP_OPTIONS = {}
 
-DEVISE_MAILER_FROM       = "please-change-me@example.com"
-LIVE_PERSONS_EMAIL       = 'help@example.com'
+DEVISE_MAILER_FROM       = "ipes4579@gmail.com"
+LIVE_PERSONS_EMAIL       = 'ipes4579@gmail.com'
 
 FACEBOOK_APP_ID          = "213491825331116"
 FACEBOOK_APP_SECRET      = "3bc4798fec78dc85235d543aa96e5f98"
 FACEBOOK_APP_PERMISSIONS = "email,offline_access,publish_stream"
 
-DEFAULT_FB_SHARE_IMAGE   = "http://localhost:3000/images/missing.png"
-DEFAULT_FB_POST_NAME     = "This Site's Name"
+DEFAULT_FB_SHARE_IMAGE   = "http://chakkomi.heroku.com/images/missing.png"
+DEFAULT_FB_POST_NAME     = "Chakkomi"
 
-DEFAULT_PAGE_TITLE       = "This Site's Name"
-DEFAULT_PAGE_DESCRIPTION = "This Site's Name"
+DEFAULT_PAGE_TITLE       = "Chakkomi"
+DEFAULT_PAGE_DESCRIPTION = "Description for Chakkomi"
 
-TWITTER_SECRET_KEY       = "L0LJGa5g4TWNUJfK9jmACNt3i2P2ykUw0TVbysQinIg"
-TWITTER_CONSUMER_KEY     = "D4sOenvRrSaI1GIGTTEeSQ"
+TWITTER_SECRET_KEY       = "WqPTsJJklVOTjBkSes1Eovu4muDnLjNG8p3QMbvQq3Q"
+TWITTER_CONSUMER_KEY     = "MihJNbixbwbJoMZTsu2w"
 
-DEFAULT_SHARE_URL        = "http://localhost:3000"
+DEFAULT_SHARE_URL        = "http://chakkomi.heroku.com/"
 
 # Load the rails application
 require File.expand_path('../application', __FILE__)
