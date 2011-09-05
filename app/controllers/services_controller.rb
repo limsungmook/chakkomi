3d645938ad11772f0a74226f04a2d330f5e8a8dc
SungMook Lim <ipes4579@gmail.com>
2011-09-06 06:30:51 +0900

    로컬 패스워드 부분 추가

--- app/controllers/services_controller.rb
+++ app/controllers/services_controller.rb
@@ -80,7 +80,7 @@ class ServicesController < ApplicationController
                 name = name[0, 39] if name.length > 39             # otherwise our user validation will hit us
 
                 # new user, set email, a random password and take the name from the authentication service
-                user = User.new :email => email, :password => SecureRandom.hex(10), :fullname => name
+                user = User.new :email => email, :password => SecureRandom.hex(10), :fullname => name, :haslocalpw => false
 
                 # add this authentication service to our new user
                 user.services.build(:provider => provider, :uid => uid, :uname => name, :uemail => email)
