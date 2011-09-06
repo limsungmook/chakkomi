cb9b0f5245b127b553c93ca2a9fc15a3268a754f
SungMook Lim <ipes4579@gmail.com>
2011-09-06 10:33:33 +0900

    seed 변경

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,14 +8,14 @@
 #   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
 #   Mayor.create(:name => 'Daley', :city => cities.first)
 
-User.delete_all
+#User.delete_all
 Post.delete_all
 Category.delete_all
 
- @user1 = User.create! :email => 'ipes4579@gmail.com', :password => '12345678', :password_confirmation => '12345678'
- @user1.confirm!
- @user2 = User.create! :email => 'chakkomi@chakkomi.com', :password => '12345678', :password_confirmation => '12345678'
- @user2.confirm!
+# @user1 = User.create! :email => 'ipes4579@gmail.com', :password => '12345678', :password_confirmation => '12345678'
+# @user1.confirm!
+# @user2 = User.create! :email => 'chakkomi@chakkomi.com', :password => '12345678', :password_confirmation => '12345678'
+# @user2.confirm!
 
 
  @category = Category.create! :name => 'intro', :detail => '착곰이 소개 페이지입니다.'
